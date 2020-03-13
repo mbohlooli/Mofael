442fa475449ce622529bad21bacf6e138e169a56
mbohlooli <bohlool82@gmail.com>
2020-03-13 22:04:23 +0330

    Main CRUD operations added for school

--- web/routes/schools.js
+++ web/routes/schools.js
@@ -6,6 +6,12 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
+router.get("/", auth, async (req, res) => {
+  const manager = req.user;
+
+  res.send(await School.find({ managerId: manager._id }));
+});
+
 router.post("/", auth, async (req, res) => {
   validate(validateSchool, req, res);
 
@@ -21,6 +27,36 @@ router.post("/", auth, async (req, res) => {
 
   await school.save();
   res.send(JSON.stringify(school));
+});
+
+router.put("/:id", auth, async (req, res) => {
+  validate(validateSchool, req, res);
+
+  const school = await School.findById(req.params.id);
+  if (!school) return res.status(404).send("مدرسه مورد نظر یافت نشد.");
+
+  const manager = req.user;
+
+  if (school.managerId != manager._id)
+    return res.status(401).send("شما اجازه ویرایش این مدرسه را ندارید.");
+
+  await school.update(_.pick(req.body, ["name", "city", "zone"]));
+
+  res.send(await School.findById(req.params.id));
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  const school = await School.findById(req.params.id);
+  if (!school) return res.status(404).send("مدرسه مورد نظر یافت نشد.");
+
+  const manager = req.user;
+
+  if (school.managerId != manager._id)
+    return res.status(401).send("شما اجازه حذف این مدرسه را ندارید.");
+
+  await school.delete();
+
+  res.send("done");
 });
 
 module.exports = router;
