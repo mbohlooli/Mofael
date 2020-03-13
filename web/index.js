c5b8b21cff88f15203b24c1105abceb840622cb3
mbohlooli <bohlool82@gmail.com>
2020-03-13 18:56:55 +0330

    School model added

--- web/index.js
+++ web/index.js
@@ -6,6 +6,7 @@ require("./startup/db")();
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/checkKey")();
+require("./startup/objectIdValidation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`listening on port ${port}`));
---
+++ web/models/school.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const schoolSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    maxlength: 50,
+    unique: true,
+    required: true
+  },
+  city: {
+    type: String,
+    minlength: 3,
+    maxlength: 30,
+    required: true
+  },
+  zone: {
+    type: Number,
+    required: true
+  },
+  managerId: {
+    type: mongoose.Types.ObjectId
+  }
+});
+
+const School = mongoose.model("Schools", schoolSchema);
+
+function validateSchool(school) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    city: Joi.string()
+      .min(3)
+      .max(30)
+      .required(),
+    zone: Joi.number().required(),
+    managerId: Joi.objectId().required()
+  };
+
+  return Joi.validate(school, schema);
+}
+
+module.exports = { School, validateSchool };
