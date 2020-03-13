58ca0be8d0c9f0935fd00152cc121b9513d138b6
mbohlooli <bohlool82@gmail.com>
2020-03-13 18:04:51 +0330

    Server errors will be logged in console

--- web/middleware/error.js
+++ web/middleware/error.js
@@ -2,5 +2,7 @@ const winston = require("winston");
 
 module.exports = function(err, req, res, next) {
   winston.error(err.message, err);
+  console.error(err);
+
   res.status(500).send("خطای سرور.");
 };
