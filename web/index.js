dfed3c7631c7b961d45fc83ec11d2331a732ffc3
mbohlooli <bohlool82@gmail.com>
2020-03-13 14:41:07 +0330

    JWT setup

--- web/index.js
+++ web/index.js
@@ -5,6 +5,7 @@ const winston = require("winston");
 require("./startup/db")();
 require("./startup/logging")();
 require("./startup/routes")(app);
+require("./startup/checkKey")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`listening on port ${port}`));
