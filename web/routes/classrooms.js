34673d3cdf9ed5738a7e623a299aa690b6737436
mbohlooli <bohlool82@gmail.com>
2020-03-15 17:49:33 +0330

    Some extra validation for classroom update

--- web/routes/classrooms.js
+++ web/routes/classrooms.js
@@ -41,17 +41,34 @@ router.post("/", [auth, manager], async (req, res) => {
 });
 
 router.put("/:id", [auth, manager], async (req, res) => {
+  validate(validateClassroom, req, res);
+
   const classroom = await Classroom.findById(req.params.id);
   if (!classroom) return res.status(404).send("کلاس مورد نظر یافت نشد.");
-  //TODO: check that the name doesnt duplicate
+
   const manager = req.user;
   const school = await School.findById(classroom.grade.schoolId);
   if (school.managerId != manager._id)
     return res.status(403).send("شما اجازه ویرایش این مدرسه را ندارید.");
 
-  // TODO: The grade can be updated if it belongs to the same person
+  let grade = classroom.grade;
+  if (req.body.gradeId) {
+    console.log("hi");
+    grade = await Grade.findById(req.body.gradeId);
+    if (!grade) return res.status(404).send("پایه مورد نظر یافت نشد.");
 
-  await classroom.update(_.pick(req.body, ["name"]));
+    const school = await School.findById(grade.schoolId);
+    if (school.managerId != manager._id)
+      return res.status(403).send("شما اجازه ویرایش این مدرسه را ندارید.");
+  }
+  const duplicateClassroom = await Classroom.findOne({
+    grade,
+    name: req.body.name
+  });
+  if (duplicateClassroom)
+    return res.status(400).send("کلاس مورد نظر قبلا ثبت شده.");
+
+  await classroom.update({ ..._.pick(req.body, ["name"]), grade });
 
   res.send(await Classroom.findById(req.params.id));
 });
