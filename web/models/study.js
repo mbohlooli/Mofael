8c707078d0eb8d63269333e28b4d82e38a6541ad
mbohlooli <bohlool82@gmail.com>
2020-03-19 12:18:50 +0330

    Useless parts removed

--- web/models/study.js
+++ web/models/study.js
@@ -9,7 +9,6 @@ const studySchema = new mongoose.Schema({
   studyHours: {
     type: [
       {
-        date: String,
         weekIndex: Number,
         lessonId: mongoose.Types.ObjectId,
         value: Number
@@ -23,8 +22,6 @@ const Study = mongoose.model("Studies", studySchema);
 
 function validateStudy(study) {
   const schema = {
-    studentId: Joi.objectId().required(),
-    date: Joi.string().required(),
     weekIndex: Joi.number().required(),
     lessonId: Joi.objectId().required(),
     value: Joi.number().required()
